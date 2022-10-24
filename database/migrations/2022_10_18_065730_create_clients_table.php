1aa0aba30744b3970b9d3ddd8d2274935969856d
bfg9kappa <bfg9kappa@gmail.com>
2022-10-24 14:18:04 +0200

    update

--- database/migrations/2022_10_18_065730_create_clients_table.php
+++ database/migrations/2022_10_18_065730_create_clients_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
+            $table->string('cognoms');
             $table->string('direccio');
             $table->int('telefon');
             $table->timestamps();
