234524e253c9897732b9e582376b3a75d9c3b142
bfg9kappa <bfg9kappa@gmail.com>
2022-12-02 10:02:24 +0100

    middle i log

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         // Middlewares
+        'is_admin' => \App\Http\Middleware\isAdminMiddleware::class,
         'role' => \App\Http\Middleware\roleMiddleware::class,
     ];
 }
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -30,14 +30,39 @@
           <li class="nav-item active">
             <a class="nav-link" href="{{ url('clients') }}">Clients</a>
           </li>
+          <!-- Menu login -->
+          @guest
+            @if (Route::has('login'))
+              <li class="nav-item active">
+                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+              </li>
+            @endif         
+            @if (Route::has('register'))
+              <li class="nav-item active">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+              </li>
+            @endif
+            @else
+            <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }}
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                </form>
+              </div>
+            </li>
+          @endguest
         </ul>
-        <span class="navbar-text">
-          <a href="/login">Accedir</a>
-        </span>
       </div>
     </nav>
     <br/>
-
     <div class="container"> 
       @yield('content')
     </div>
