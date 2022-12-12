c39b316a1f96f21487855ba185ab7bb88b0f996c
Victor <victor.porras.work@gmail.com>
2022-12-12 14:20:01 +0100

    portada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,12 @@
 @extends('template')
 @section('content')
 
-<h4>Menjars APP</h4>
-
+<h4>Gestió de Menjars APP</h4>
 <p>
-    Aplicacio per la gestio de plats de un restaurant.
+    Aplicació per la gestio de plats de un restaurant.
 </p>
-<img src="https://i.pinimg.com/564x/84/d1/fe/84d1feac80ae33993a30deedfc1d9e7b.jpg" alt="Meme rana" width="auto" height="auto">
+<div align="center">
+    <img src="{{url('img/Imatge1.png')}}" with="540" height="540" alt="Portada">
+</div>
 
 @endsection
