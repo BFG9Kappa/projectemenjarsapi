bd015452eb6d686a10cf09df1693aa4a69e32248
josep <bfg9kappa@gmail.com>
2023-01-23 23:07:50 +0100

    temporal index

--- resources/views/comandes/api/index.blade.php
+++ resources/views/comandes/api/index.blade.php
@@ -1,157 +1,128 @@
-<!DOCTYPE html>
-    <head>
-    </head>
-    <body>
-        CRUD COMANDAS    
-        <div>
-            <input type="text" id="comandaNameInput"><!DOCTYPE html>
-            <button id="saveButton">Save</button>
-        </div>
-        <table>
-            <thead>
-               <tr>
-                <th>id</th>
+@extends('template')
+@section('content')
+
+<!--
+<input type="text" id="nameInput">
+<input type="text" id="preuInput">
+<input type="text" id="estatInput">
+<button class="btn btn-primary btn-sm" id="saveButton">Desar</button>
+-->
+
+<div class="table-responsive">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
                 <th>Nom</th>
-               </tr>
-            </thead>
-            <tbody id="taula">
-          
+                <th>Preu</th>
+                <th>Estat</th>
+            </tr>
+        </thead>
+        <tbody id="taula">
+        </tbody>
+    </table>
+</div>
 
-            </tbody>
-        </table>
-    </body>
-</html>
-<script type="text/javascript">
-            //console.log('hola');
+<script>
+    const table = document.getElementById('taula');
 
-            const table = document.getElementById('taula')
-            const comandaNameInput = document.getElementById('comandaNameInput')
-            const saveButton = document.getElementById('saveButton')
-            saveButton.addEventListener('click', saveComanda)
-           
-            const url = 'http://127.0.0.1:8000/api/comandes/';
+    const comandaNameInput = document.getElementById('nameInput');
+    const comandaPreuInput = document.getElementById('preuInput');
+    const comandaEstatInput = document.getElementById('estatInput');
 
-            async function saveComanda(event){
-                console.log('desar');
-                var newComanda = {
-                    "nom" : comandaNameInput.value
-                }
+    const saveButton = document.getElementById('saveButton');
+    saveButton.addEventListener('click', saveData);
+    const url = 'http://127.0.0.1:8000/api/comandes/';
 
-                try{
-                    const response = await fetch(url,
-                        { method: 'POST',
-                            headers: {
-                                'Content-type': 'application/json',
-                                'Accept' : 'application/json'
-                            },
-                            body: JSON.stringify(newComanda)
-                        }
-                      )
-                      const data = await response.json();
-                      if(response.ok){
-                        afegirFila(data.data); 
-
-
-
-                      }
-
-                    }
-                catch(error){
-
-                }
-
-            }
-           
-            function afegirFila(row){
-                const rowElement = document.createElement("tr");
-                        rowElement.setAttribute('id', row.id)
-                        const idCell = document.createElement("td");
-                        idCell.textContent = row.id;
-                        const nomCell = document.createElement("td");
-                        nomCell.textContent = row.nom;
-                        
-                        const operationsCell = document.createElement("td");
-                        const deleteButton = document.createElement("button");
-                        deleteButton.innerHTML = "Esborrar";
-                        deleteButton.addEventListener('click', deleteComanda);
-                        operationsCell.appendChild(deleteButton);
-
-                        rowElement.appendChild(idCell);
-                        rowElement.appendChild(nomCell);
-                        rowElement.appendChild(operationsCell);
-
-                        table.appendChild(rowElement);
-
-
-
-            }
-           
-            async function deleteComanda(event){
-
-                //console.log(id);
-                //console.log(event.target.parentNode.parentNode.id);
-                //console.log(event.target.closest('tr').id);
-
-                try {
-                    const id = event.target.closest('tr').id
-                    response = await fetch(url+''+id, { method: 'DELETE'})
-                    const json = await response.json();
-                    if(response.ok){ //codi 200,...
-                        const row = document.getElementById(id)
-                        row.remove()
-                    }
-                        else{
-                        console.log('Error esborrant')
-
-                        }
-                    }
-
-                 catch(error){
-                    console.log('error xarxa')
-                 }   
-
-            }
-             
-            async function loadIntoTable(url){
-
-                try{
-
-                    const response = await fetch(url);
-                    const json = await response.json();
-
-                    const rows = json.data;
-                    //console.log(rows)
-                    //const table = document.getElementById('taula');
-                  
-                    for (var row of rows){
-                       
-                        const rowElement = document.createElement("tr");
-                        rowElement.setAttribute('id', row.id)
-                        const idCell = document.createElement("td");
-                        idCell.textContent = row.id;
-                        const nomCell = document.createElement("td");
-                        nomCell.textContent = row.nom;
-                        
-                        const operationsCell = document.createElement("td");
-                        const deleteButton = document.createElement("button");
-                        deleteButton.innerHTML = "Esborrar";
-                        deleteButton.addEventListener('click', deleteComanda);
-                        operationsCell.appendChild(deleteButton);
-
-                        rowElement.appendChild(idCell);
-                        rowElement.appendChild(nomCell);
-                        rowElement.appendChild(operationsCell);
-
-                        table.appendChild(rowElement);
-                    }
-                }
-                catch (error){
-
-
-            }
+    async function saveData(event) {
+        let newComanda = {
+            "nom": comandaNameInput.value,
+            "preu": comandaPreuInput.value,
+            "estat": comandaEstatInput.value
         }
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(newComanda)
+            });
+            const data = await response.json();
+            if (response.ok) {
+                addRow(data.data);
+            }
+        } catch (error) {
+            //
+        }
+    }
 
-            loadIntoTable(url);
+    function addRow(row) {
+        const rowElement = document.createElement("tr");
+        rowElement.setAttribute('id', row.id);
+        const idCell = document.createElement("td");
+        idCell.textContent = row.id;
+        const nomCell = document.createElement("td");
+        nomCell.textContent = row.nom;
+        const operationsCell = document.createElement("td");
+        const deleteButton = document.createElement("button");
+        deleteButton.innerHTML = "Esborrar";
+        deleteButton.addEventListener('click', deleteComanda);
+        operationsCell.appendChild(deleteButton);
+        rowElement.appendChild(idCell);
+        rowElement.appendChild(nomCell);
+        rowElement.appendChild(operationsCell);
+        table.appendChild(rowElement);
+    }
 
+    async function deleteComanda(event) {
+        try {
+            const id = event.target.closest('tr').id;
+            const response = await fetch(url + id, {
+                method: 'DELETE'
+            });
+            const json = await response.json();
+            if (response.ok) { 
+                const row = document.getElementById(id);
+                row.remove();
+            } else {
+                console.log('Error esborrant');
+            }
+        } catch (error) {
+            console.log('error xarxa');
+        }
+    }
 
-        </script>
+    async function loadIntoTable(url) {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            const rows = json.data;
+            for (let row of rows) {
+                const rowElement = document.createElement("tr");
+                rowElement.setAttribute('id', row.id);
+                const idCell = document.createElement("td");
+                idCell.textContent = row.id;
+                const nomCell = document.createElement("td");
+                nomCell.textContent = row.nom;
+                const operationsCell = document.createElement("td");
+                const deleteButton = document.createElement("button");
+                deleteButton.innerHTML = "Esborrar";
+                deleteButton.addEventListener('click', deleteComanda);
+                operationsCell.appendChild(deleteButton);
+                rowElement.appendChild(idCell);
+                rowElement.appendChild(nomCell);
+                rowElement.appendChild(operationsCell);
+                table.appendChild(rowElement);
+            }
+        } catch (error) {
+            //
+        }
+    }
+
+    loadIntoTable(url);
+
+</script>
+
+@endsection
