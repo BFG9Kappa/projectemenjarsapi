524f729dcb98a39ef2b2faa84544e7bc9a72de1a
Victor <victor.porras.work@gmail.com>
2023-01-27 10:49:40 +0100

    ultima actu victor

--- resources/views/clients/api/index.blade.php
+++ resources/views/clients/api/index.blade.php
@@ -1,36 +1,72 @@
 @extends('template')
 @section('content')
 
-<div class="table-responsive">
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Nom</th>
-        <th scope="col">Cognoms</th>
-        <th scope="col">Direccio</th>
-        <th scope="col">Telefon</th>
-        <th scope="col" colspan="2">Operacions</th>
-      </tr>
-    </thead>
-    <tbody id="taula">
-    </tbody>
-  </table>
+<div>
+    <h4>Crud</h4>
+    <label for="nameInput">Nom</label>
+    <input type="text" id="nameInput">
+    <label for="lastnameInput">Cognoms</label>
+    <input type="text" id="lastnameInput">
+    <label for="adressInput">Direcció</label>
+    <input type="text" id="adressInput">
+    <label for="phoneInput">Telèfon</label>
+    <input type="text" id="phoneInput">
+    <button class="btn btn-primary btn-sm" id="saveButton">Desar</button>
+    <br/>
+    <br/>
 </div>
 
-<script>
+<div id="errors" class="alert alert-danger" role="alert"></div>
+<div class="table-responsive">
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Cognoms</th>
+                <th>Direcció</th>
+                <th>Teléfon</th>   
+            </tr>
+        </thead>
+        <tbody id="taula">
+        </tbody>
+    </table>
+</div>
+
+<script type="text/javascript">
+
     const table = document.getElementById('taula');
+    const divErrors = document.getElementById('errors');
+	divErrors.style.display = "none"    
 
-    //const comandaNameInput = document.getElementById('comandaNameInput');
-    //const saveButton = document.getElementById('saveButton');
-    //saveButton.addEventListener('click', saveComanda);
+    const clientNameInput = document.getElementById('nameInput');
+    const clientLastnameInput = document.getElementById('lastnameInput');
+    const clientAdressInput = document.getElementById('adressInput');
+    const clientPhoneInput = document.getElementById('phoneInput');
 
+
+    const saveButton = document.getElementById('saveButton');
+    saveButton.addEventListener('click', saveData);
     const url = 'http://127.0.0.1:8000/api/clients/';
 
-    /*
-    async function saveComanda(event) {
-        let newComanda = {
-            "nom": comandaNameInput.value
+    function showErrors(errors) {
+        divErrors.style.display = "block"
+        divErrors.innerHTML = "";
+        const ul = document.createElement("ul")
+        for(const error of errors) {				
+                const li = document.createElement("li");				
+                li.textContent = error;				
+                ul.appendChild(li);			
+        }
+        divErrors.appendChild(ul)
+    }
+    
+    async function saveData(event) {
+        let newClient = {
+            "nom": clientNameInput.value,
+            "cognoms": clientLastnameInput.value,
+            "direccio": clientAdressInput.value,
+            "telefon": clientPhoneInput.value
         }
         try {
             const response = await fetch(url, {
@@ -39,37 +75,53 @@
                     'Content-type': 'application/json',
                     'Accept': 'application/json'
                 },
-                body: JSON.stringify(newComanda)
+                body: JSON.stringify(newIngredient)
             });
             const data = await response.json();
             if (response.ok) {
-                addRows(data.data);
+                addRow(data.data);
+            } else {
+                showErrors(data.data)
             }
         } catch (error) {
-            //
+            error.innerHTML = "S'ha produit un error inesperat" 
         }
     }
-    */
 
-    function addRows(row) {
+    function addRow(row) {
         const rowElement = document.createElement("tr");
         rowElement.setAttribute('id', row.id);
         const idCell = document.createElement("td");
         idCell.textContent = row.id;
         const nomCell = document.createElement("td");
         nomCell.textContent = row.nom;
+        
+        const cognomsCell = document.createElement("td");
+        cognomsCell.textContent = row.cognoms;
+
+        const direccioCell = document.createElement("td");
+        direccioCell.textContent = row.direccio;
+
+        const telefonCell = document.createElement("td");
+        telefonCell.textContent = row.telefon;
+
         const operationsCell = document.createElement("td");
         const deleteButton = document.createElement("button");
         deleteButton.innerHTML = "Esborrar";
-        deleteButton.addEventListener('click', deleteComanda);
+        deleteButton.addEventListener('click', deleteRow);
         operationsCell.appendChild(deleteButton);
         rowElement.appendChild(idCell);
         rowElement.appendChild(nomCell);
+
+        rowElement.appendChild(cognomsCell);
+        rowElement.appendChild(direccioCell);
+        rowElement.appendChild(telefonCell);
+
         rowElement.appendChild(operationsCell);
         table.appendChild(rowElement);
     }
 
-    async function deleteComanda(event) {
+    async function deleteRow(event) {
         try {
             const id = event.target.closest('tr').id;
             const response = await fetch(url + id, {
@@ -99,18 +151,32 @@
                 idCell.textContent = row.id;
                 const nomCell = document.createElement("td");
                 nomCell.textContent = row.nom;
+
+                const cognomsCell = document.createElement("td");
+                cognomsCell.textContent = row.cognoms;
+
+                const direccioCell = document.createElement("td");
+                direccioCell.textContent = row.direccio;
+
+                const telefonCell = document.createElement("td");
+                telefonCell.textContent = row.telefon;
+
                 const operationsCell = document.createElement("td");
                 const deleteButton = document.createElement("button");
                 deleteButton.innerHTML = "Esborrar";
-                deleteButton.addEventListener('click', deleteComanda);
+                deleteButton.addEventListener('click', deleteRow);
                 operationsCell.appendChild(deleteButton);
                 rowElement.appendChild(idCell);
                 rowElement.appendChild(nomCell);
+
+                rowElement.appendChild(cognomsCell);
+                rowElement.appendChild(direccioCell);
+                rowElement.appendChild(telefonCell);
                 rowElement.appendChild(operationsCell);
                 table.appendChild(rowElement);
             }
         } catch (error) {
-            //
+            errors.innerHTML = "No es pot accedir a la base de dades"
         }
     }
 
