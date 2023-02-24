fd4c398d84eb4e07680547fad000a6441186fb0d
josep <bfg9kappa@gmail.com>
2023-02-24 01:23:31 +0100

    factory client, i test inicial

--- tests/Feature/API/ClientsTest.php
+++ tests/Feature/API/ClientsTest.php
@@ -6,17 +6,28 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+use App\Models\Client;
+
 class ClientsTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+     * @test
+     * @group api
+     **/
+    public function llistat_carregue_correctament()
     {
-        $response = $this->get('/');
-
+        $clients = Client::factory()->count(5)->create();
+        $response = $this->get('/api/clients');
         $response->assertStatus(200);
+        foreach ($clients as $client) {
+            $response->assertJsonFragment([
+                'nom' => $client->nom,
+                'cognoms' => $client->cognoms,
+                'direccio' => $client->direccio,
+                'telefon' => $client->telefon
+            ]);
+        }
     }
+
 }
