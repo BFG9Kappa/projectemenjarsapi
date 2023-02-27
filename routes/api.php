66fedb93f3458959944c60999e265b940e6b798c
josep <bfg9kappa@gmail.com>
2023-02-27 22:41:46 +0100

    update tokens

--- routes/api.php
+++ routes/api.php
@@ -14,27 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*
-Route::middleware('auth:sanctum')->get('/clients', function (Request $request) {
-    return $request->user();
-});
-*/
-
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-/*
-//
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/clients', function (Request $request) {
     return $request->user();
 });
-*/
 
 Route::middleware('auth:sanctum')->group( function () {
-    //Afegir rutes a protegir aqui
-
+    // Afegir rutes a protegir
 });
 
 Route::resource('/plats', App\Http\Controllers\Api\PlatController::class);
