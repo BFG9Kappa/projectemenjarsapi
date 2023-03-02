1a5bd0d6928e6f4551d56f2db9c7c496bf854816
bfg9kappa <bfg9kappa@gmail.com>
2023-03-02 08:22:22 +0100

    update token :)

--- routes/api.php
+++ routes/api.php
@@ -18,15 +18,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:sanctum')->get('/clients', function (Request $request) {
-    return $request->user();
-});
-
 Route::middleware('auth:sanctum')->group( function () {
     // Afegir rutes a protegir
+    Route::resource('/clients', App\Http\Controllers\Api\ClientController::class);
 });
 
 Route::resource('/plats', App\Http\Controllers\Api\PlatController::class);
 Route::resource('/ingredients', App\Http\Controllers\Api\IngredientController::class);
 Route::resource('/comandes', App\Http\Controllers\Api\ComandesController::class);
-Route::resource('/clients', App\Http\Controllers\Api\ClientController::class);
+
