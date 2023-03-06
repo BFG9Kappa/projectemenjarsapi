320ff9b84b6b453a818dd0f76da877a3b0019087
Victor <victor.porras.work@gmail.com>
2023-03-06 14:10:30 +0100

    n a m

--- resources/views/comandes/api/showPlats.blade.php
+++ resources/views/comandes/api/showPlats.blade.php
@@ -1,2 +1,29 @@
 <p>Dios<p>
 <!-- Hacer un fetch json de plats, y mostrat las dos listas attach y detach-->
+
+@extends('template')
+@section('content')
+
+<script type="text/javascript">
+
+//var async = require('asyncawait/async');
+//var await = require('asyncawait/await');
+
+
+async function loadIntoContainer() {
+		try {
+			const response = await fetch(url);
+			const json = await response.json();
+            const plats= json.data;
+            nom.innerText= plats.nom;
+
+        }
+		catch(error) {
+			error.innerHTML = "No es pot accedir a la base de dades";
+		}
+	}
+
+    loadIntoContainer()
+
+</script>
+@endsection
